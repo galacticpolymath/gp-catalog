0ddfd4c0175e82c6ec72f72f6264c1cbb14d9e50
Patrick Cheng <pcheng86@gmail.com>
2021-12-14 18:24:39 -0800

    adfs

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from os import listdir
 from os.path import isfile, join
 mypath = "./"
-onlyfiles = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
+onlyfiles = [f for f in listdir(mypath) if not isfile(join(mypath, f)) and f[0] != "."]
 
 print(onlyfiles)
