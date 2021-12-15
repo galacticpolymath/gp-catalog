707de042182fc06de9ec906f84943ce7b928bca9
Patrick Cheng <pcheng86@gmail.com>
2021-12-14 18:19:29 -0800

    asdf

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from os import listdir
 from os.path import isfile, join
 mypath = "./"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+onlyfiles = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
 
 print(onlyfiles)
