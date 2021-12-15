17c7668ef157929b1603c82eeb62dff18eb9ab0d
Patrick Cheng <pcheng86@gmail.com>
2021-12-14 18:33:46 -0800

    asdfads

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
 from os import listdir
 from os.path import isfile, join
 mypath = "./"
-onlyfiles = [f for f in listdir(mypath) if not isfile(join(mypath, f)) and f[0] != "."]
+lessons = [f for f in listdir(mypath) if not isfile(join(mypath, f)) and f[0] != "."]
 
-print(onlyfiles)
+for lesson in lessons:
+    # permalink??
+    # crawl lesson-plans.json for Title, SponsoredBy, and Subtitle
+    # add each to a string if present
+    # add string into dictionary?
+    # include entire lesson plan?
+    # include pics?
+    pass
