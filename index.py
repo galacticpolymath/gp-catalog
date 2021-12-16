455ddfa4df09db86af479bf70068c4ca455c1431
Patrick Cheng <pcheng86@gmail.com>
2021-12-16 09:03:54 -0800

    index script

--- index.py
+++ index.py
@@ -1,13 +1,25 @@
 from os import listdir
-from os.path import isfile, join
-mypath = "./"
-lessons = [f for f in listdir(mypath) if not isfile(join(mypath, f)) and f[0] != "."]
+from os.path import isfile, join, exists
+import json
+
+search_attributes = keys = ['ShortTitle', 'LastUpdated', 'PublishedOn', 'Title', 'Subtitle', 'SponsoredBy', 'Standards', 'Keywords']
+lessons = [f for f in listdir('./') if not isfile(join(mypath, f)) and f[0] != "."]
+index = []
+
+assert len(lessons)!= 0, 'No lessons found...'
 
 for lesson in lessons:
-    # permalink??
-    # crawl lesson-plans.json for Title, SponsoredBy, and Subtitle
-    # add each to a string if present
-    # add string into dictionary?
-    # include entire lesson plan?
-    # include pics?
-    pass
+    json_path = lesson + '/LESSON.json'
+
+    if exists(json_path):
+        with open(json_path) as file:
+            lesson_json = json.load(file)
+            filtered_attributes = [x for x in search_attributes if x in lesson_json.keys()]
+            search_terms = {attribute: lesson_json[attribute] for attribute in filtered_attributes}
+            index += search_terms
+
+    else:
+        print("No LESSON.json found...")
+
+with open('index.json', 'w') as json_file:
+    json.dump(index, json_file)
